5862e63c4fec391e4c0a49a4b2e96639a718baa7
nTudor0911 <40658894+ntudor11@users.noreply.github.com>
2019-05-18 13:46:33 +0200

    Shell sorting

---
+++ Sorting/Shell.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Shell {
+  public static void sort(Comparable[] a) {
+    int N = a.length;
+
+    int h = 1;
+    while (h < N/3) {
+      h = 3*h + 1; // 1, 4, 13, 40, 121, etc...
+    }
+
+    while (h >= 1) { // h sort the array
+      for (int i = h; i < N; i++) {
+        for (int j = i; j >= h && less(a[j], a[j-h]); j-=h) { // insertion sort - decrement j each time by h
+          exch(a, j, j-h);
+        }
+      }
+      h = h/3;
+    }
+  }
+
+  private static boolean less(Comparable v, Comparable w) {
+    return v.compareTo(w) < 0;
+  }
+
+  private static void exch(Object[] a, int i, int j) {
+    Object swap = a[i];
+    a[i] = a[j];
+    a[j] = swap;
+  }
+
+  private static void show(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+        StdOut.println(a[i]);
+    }
+  }
+
+  public static void main(String[] args) {
+    String[] a = StdIn.readAllStrings();
+    Shell.sort(a);
+    show(a);
+  }
+}
